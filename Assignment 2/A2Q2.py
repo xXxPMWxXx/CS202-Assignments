733a16adec5e56884934efeea9b4e4a7a9dad85d
xXxPMWxXx <panmingwei123@gmail.com>
2024-02-27 13:37:55 +0800

    update time complexity for Q2

--- Assignment 2/A2Q2.py
+++ Assignment 2/A2Q2.py
@@ -2,11 +2,13 @@ import sys
 
 lcslen = 0
 # Method to get the longest bitonic sequence
-def lbs(nums):
+# Overall Time Complexity for longest_bitonic_subsequence method is : O(n^2), where n is the length of nums
+def longest_bitonic_subsequence(nums):
     n = len(nums)
 
     increasing = [0] * n
     increasing[0] = 1
+    # Time complexity: O(n^2)
     for i in range(1, n):
         for j in range(i):
             if nums[j] < nums[i] and increasing[j] > increasing[i]:
@@ -15,6 +17,7 @@ def lbs(nums):
 
     decreasing = [0] * n
     decreasing[n - 1] = 1
+    # Time complexity: O(n^2)
     for i in reversed(range(n - 1)):
         for j in reversed(range(i + 1, n)):
             if nums[j] < nums[i] and decreasing[j] > decreasing[i]:
@@ -23,11 +26,14 @@ def lbs(nums):
 
     # check the max for increasing and decreasing
     max_lbs = 1
+    # Time complexity: O(n)
     for i in range(n):
         max_lbs = max(max_lbs, increasing[i] + decreasing[i] - 1)
     return max_lbs
 
-#To get the longest common subsequence
+# refer from https://www.geeksforgeeks.org/longest-common-subsequence-dp-4/
+# To get the longest common subsequence using memoization
+# Time complexity: O(len1 * len2)
 def lcs(arr1, arr2, len1, len2, i, j,dp):
     if i == len1 or j == len2:
         dp[i][j] = 0
@@ -40,16 +46,22 @@ def lcs(arr1, arr2, len1, len2, i, j,dp):
         dp[i][j] = max(lcs(arr1, arr2, len1, len2, i + 1, j,dp), lcs(arr1, arr2, len1, len2, i, j + 1,dp))
     return dp[i][j]
 
+# Overall Time Complexity : O(u * m * n); 
+# where 'u' is the length of the unique elements, when two input arrays are combined
+# 'm' is the length of array 1
+# 'n' is the length of array 2
 # refer from https://www.geeksforgeeks.org/print-longest-common-sub-sequences-lexicographical-order/
 def getAllSubsequences(arr1, arr2, len1, len2, data, indx1, indx2, currlcs, dp, highest_common):
     if currlcs == lcslen:
         # To update the highest common subsequence
-        current_common_count = lbs(data[:currlcs])
+        current_common_count = longest_bitonic_subsequence(data[:currlcs])
         if current_common_count > highest_common[0]:
             highest_common[0] = current_common_count
         return
 
+    # combine the two arrays and get all the unique elements
     unique_elements = sorted(set(arr1[indx1:]) & set(arr2[indx2:]))
+    # Time complexity for the nested loops : O(len_of_unique_elements * len1 * len2)
     for num in unique_elements:
         done = False
         for i in range(indx1, len1):
@@ -63,6 +75,7 @@ def getAllSubsequences(arr1, arr2, len1, len2, data, indx1, indx2, currlcs, dp, 
                 if done:
                     break
 
+
 def LCMS(arr1, arr2):
     global lcslen
     len1, len2 = len(arr1), len(arr2)
@@ -70,8 +83,8 @@ def LCMS(arr1, arr2):
     #dp for the lcs 2D array of length larger_len
     dp = [[-1 for _ in range(larger_len)] for _ in range(larger_len)]
     lcslen = lcs(arr1, arr2, len1, len2, 0, 0, dp)
-    #date used to get all the subsequences
-    data = [0 for _ in range(larger_len)]
+    #date used to store the longest common subsequences
+    data = [-1 for _ in range(lcslen)]
     #To store the highest common mountain subsequences
     highest_common = [0]
     getAllSubsequences(arr1, arr2, len1, len2, data, 0, 0, 0, dp, highest_common)
@@ -81,6 +94,7 @@ num_pair = int(sys.stdin.readline())
 for _ in range(num_pair):
     a = [int(s) for s in sys.stdin.readline().split()]
     b = [int(s) for s in sys.stdin.readline().split()]
+    # To only consider common elements in the given sequence, to improve the runtimes
     common = set(a) & set(b)
     a,b = [x for x in a if x in common], [x for x in b if x in common]
     LCMS(a,b)
--- Assignment 2/test.py
+++ Assignment 2/test.py
@@ -1,88 +1,28 @@
-
-import sys
-
-lcslen = 0
-# Method to get the longest bitonic sequence
-def lbs(arr):
-    n = len(arr)
-    # allocate memory for LIS[] and initialize LIS values as 1
-    # for all indexes
-    lis = [1 for i in range(n+1)]
+def lcs(X, Y, m, n):
  
-    # Compute LIS values from left to right
-    for i in range(1 , n):
-        for j in range(0 , i):
-            if ((arr[i] > arr[j]) and (lis[i] < lis[j] +1)):
-                lis[i] = lis[j] + 1
+    # Declaring the array for storing the dp values
+    L = [[None]*(n+1) for i in range(m+1)]
  
-    # allocate memory for LDS and initialize LDS values for
-    # all indexes
-    lds = [1 for i in range(n+1)]
-     
-    # Compute LDS values from right to left
-    for i in reversed(range(n-1)): #loop from n-2 downto 0
-        for j in reversed(range(i-1 ,n)): #loop from n-1 downto i-1
-            if(arr[i] > arr[j] and lds[i] < lds[j] + 1):
-                lds[i] = lds[j] + 1
+    # Following steps build L[m+1][n+1] in bottom up fashion
+    # Note: L[i][j] contains length of LCS of X[0..i-1]
+    # and Y[0..j-1]
+    for i in range(m+1):
+        for j in range(n+1):
+            if i == 0 or j == 0:
+                L[i][j] = 0
+            elif X[i-1] == Y[j-1]:
+                L[i][j] = L[i-1][j-1] + 1
+            else:
+                L[i][j] = max(L[i-1][j], L[i][j-1])
+ 
+    # L[m][n] contains the length of LCS of X[0..n-1] & Y[0..m-1]
+    return L[m][n]
 
-    # Return the maximum value of (lis[i] + lds[i] - 1)
-    maximum = lis[0] + lds[0] - 1
-    for i in range(1 , n):
-        maximum = max((lis[i] + lds[i]-1), maximum)
-    return maximum
+# Driver code
+if __name__ == '__main__':
+    a = [239, 27, 116, 26, 55, 214, 243, 43, 150, 96, 150, 45, 175, 236, 150, 206, 43, 115, 189, 32, 254, 255, 239, 16, 234, 254, 231, 234, 243, 220, 250, 242, 254, 153, 209, 225, 214, 32, 194, 185, 217, 249, 55, 250, 171, 209, 220, 254, 190, 94, 55, 147, 96, 104, 32]
+    b = [27, 26, 147, 115, 243, 239, 16, 43, 234, 96, 190, 32, 45, 209, 175, 150, 206, 116, 254, 104, 189, 225, 236, 255, 231, 234, 254, 250, 254, 209, 214, 185, 217, 171, 249, 231, 220, 250, 94, 242, 153, 96, 185, 250, 55, 194, 32, 255]
 
-def lcs(arr1, arr2, len1, len2, i, j,dp):
-    if i == len1 or j == len2:
-        dp[i][j] = 0
-        return dp[i][j]
-    if dp[i][j] != -1:
-        return dp[i][j]
-    if arr1[i] == arr2[j]:
-        dp[i][j] = 1 + lcs(arr1, arr2, len1, len2, i + 1, j + 1,dp)
-    else:
-        dp[i][j] = max(lcs(arr1, arr2, len1, len2, i + 1, j,dp), lcs(arr1, arr2, len1, len2, i, j + 1,dp))
-    return dp[i][j]
-
-# refer from https://www.geeksforgeeks.org/print-longest-common-sub-sequences-lexicographical-order/
-def getAllSubsequences(arr1, arr2, len1, len2, data, indx1, indx2, currlcs , dp , highest_common):
-    if currlcs == lcslen:
-        # To update the highest common subsequence
-        current_common_count = lbs(data[:currlcs])
-        if current_common_count > highest_common[0]:
-            highest_common[0] = current_common_count
-        return
-
-    unique_elements = sorted(set(arr1[indx1:]) & set(arr2[indx2:]))
-    for num in unique_elements:
-        done = False
-        for i in range(indx1, len1):
-            if num == arr1[i]:
-                for j in range(indx2, len2):
-                    if num == arr2[j] and dp[i][j] == lcslen - currlcs:
-                        data[currlcs] = num
-                        getAllSubsequences(arr1, arr2, len1, len2, data, i + 1, j + 1, currlcs + 1, dp, highest_common)
-                        done = True
-                        break
-                if done:
-                    break
-
-def LCMS(arr1, arr2):
-    global lcslen
-    len1, len2 = len(arr1), len(arr2)
-    larger_len = max(len1, len2) + 1
-    #dp for the lcs
-    dp = [[-1 for _ in range(larger_len)] for _ in range(larger_len)]
-    lcslen = lcs(arr1, arr2, len1, len2, 0, 0,dp)
-    #date used to get all the subsequences
-    data = [0 for _ in range(larger_len)]
-    highest_common = [0]
-    getAllSubsequences(arr1, arr2, len1, len2, data, 0, 0, 0,dp, highest_common)
-    print(highest_common[0])
-
-num_pair = int(sys.stdin.readline())
-for _ in range(num_pair):
-    a = [int(s) for s in sys.stdin.readline().split()]
-    b = [int(s) for s in sys.stdin.readline().split()]
-    common = set(a) & set(b)
-    a,b = [x for x in a if x in common], [x for x in b if x in common]
-    LCMS(a,b)
+    m = len(a)
+    n = len(b)
+    print("Length of LCS is", lcs(a, b, m, n))
